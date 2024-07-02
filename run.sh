aeaf5b4a5b767ed9978a825975f58abb0b4644d4
Max Shinn <max@maxshinnpotential.com>
2024-07-02 10:36:30 +0100

    Run script updates

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-python3 -m flask --app controller run
-
+#python3 -m flask --app controller run --host=0.0.0.0 --port=80
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+waitress-serve --host rota.cortexlab.net --port 8080 controller:app
